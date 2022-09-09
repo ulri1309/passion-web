70daf978901e7c75f6563ef55ba8a4115baad387
Laura <laur730t@stud.kea.dk>
2022-09-09 14:52:10 +0200

    fetch

---
+++ script.js
@@ -0,0 +1,26 @@
+// Henter URL Link
+const url = "https://hiking-3bbe.restdb.io/rest/hiking";
+
+// Henter API
+const options = {
+  header: { "x-apikey": "631b3153fdc15b0265f17281" },
+};
+
+// Globale variabel
+let lande;
+
+// // Tjekker at DOM er loaded
+// document.addEventListener("DOMContentLoaded", start);
+// function start() {
+//   console.log("loaded");
+// }
+
+// Vi henter data med fetch funktion vi asynkron funktion
+async function hentData() {
+  const respons = await fetch(url, options);
+  lande = await respons.json();
+  console.log("land", lande);
+  vis();
+}
+
+hentData();
