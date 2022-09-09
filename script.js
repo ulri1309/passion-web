ee1dadb7fa93ed3a21bfe1622b966d09c8051330
Laura <laur730t@stud.kea.dk>
2022-09-09 14:59:56 +0200

    fetch virker

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ const url = "https://hiking-3bbe.restdb.io/rest/hiking";
 
 // Henter API
 const options = {
-  header: { "x-apikey": "631b3153fdc15b0265f17281" },
+  headers: { "x-apikey": "631b3153fdc15b0265f17281" },
 };
 
 // Globale variabel
 let lande;
 
-// // Tjekker at DOM er loaded
-// document.addEventListener("DOMContentLoaded", start);
-// function start() {
-//   console.log("loaded");
-// }
+// Tjekker at DOM er loaded
+document.addEventListener("DOMContentLoaded", start);
+function start() {
+  console.log("loaded");
+}
 
 // Vi henter data med fetch funktion vi asynkron funktion
 async function hentData() {
