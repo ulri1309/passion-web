59b44a87fa389eeae7ffae654b1c3dcef6a24ea5
Laura <laur730t@stud.kea.dk>
2022-09-12 09:04:43 +0200

    loop view

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// ************ Indsamling af data ************************
 // Henter URL Link
 const url = "https://hiking-3bbe.restdb.io/rest/hiking";
 
@@ -7,7 +8,7 @@ const options = {
 };
 
 // Globale variabel
-let lande;
+let hikes;
 
 // Tjekker at DOM er loaded
 document.addEventListener("DOMContentLoaded", start);
@@ -15,11 +16,79 @@ function start() {
   console.log("loaded");
 }
 
-// Vi henter data med fetch funktion vi asynkron funktion
+// Vi henter data med fetch funktion via asynkron funktion
 async function hentData() {
   const respons = await fetch(url, options);
-  lande = await respons.json();
-  console.log("land", lande);
+  hikes = await respons.json();
+  console.log("lande", hikes);
 }
 
+// ************** filtrering ****************************
+// laver variabel, som sættes lige med alle -> så det er den generelle indstilling, når man kommer ind på siden.
+let filter = "alle";
+
+// Laver konstant der gør vi får fat i alle knapperne
+const filterKnapper = document.querySelectorAll("nav button");
+
+// Gør knapperne klikbarer og kalder anonym funktion
+filterKnapper.forEach((knap) => knap.addEventListener("click", filtrerLande));
+
+// konstant til h2 tekst
+const textOverskrift = document.querySelector("header h2");
+
+// kalder på filtrerlande
+function filtrerLande() {
+  // finder værdien der ligger i knappens data-attribut.
+  filter = this.dataset.land;
+
+  // fjerner klassen valgt fra alle
+  document.querySelector(".valgt").classList.remove("valgt");
+
+  // tilføjer klassen valgt til kategorierne der bliver klikket på
+  this.classList.add("valgt");
+
+  //Gør at h2 overskriften passer til den valgte kategori
+  textOverskrift.textContent = this.textContent;
+
+  // Kalder visHikes funktionen på ny
+  visHikes();
+}
+
+// ***************** Loop view *******************************
+// konstant for der indholdet skal i
+const indhold = document.querySelector("section");
+
+// konstant for templaten
+const template = document.querySelector("template").content;
+
+function visHikes() {
+  console.log(hikes);
+  // fjerner indholdet i templaten
+  indhold.textContent = "";
+
+  hikes.forEach((hike) => {
+    console.log("land", hike.land);
+    if (filter == hike.land || filter == "alle") {
+      const klon = template.cloneNode(true);
+      // Laver artiklen klikbar, for at kunne lave nyt vindue
+      klon
+        .querySelector("article")
+        .addEventListener("click", () => visDetaljer(hike));
+
+      // Indhold
+      klon.querySelector("img").src = "billeder/" + hike.image + "-md.webp";
+      klon.querySelector("h2").textContent = hike.land;
+      klon.querySelector("h3").textContent = hike.mountain;
+      klon.querySelector("p").textContent = hike.shortDescription;
+      indhold.appendChild(klon);
+    }
+  });
+}
+
+// Gør vi åbner en ny side
+function visDetaljer(hike) {
+  location.href = `enkeltside.html?id=${hike._id}`;
+}
+
+// Henter data
 hentData();
