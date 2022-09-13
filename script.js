4d1cd6b588ddeab58f7abe126c384abd90fcefd8
Felicia Hetman <feli5659@stud.kea.dk>
2022-09-13 21:39:27 +0200

    felicia styler

--- script.js
+++ script.js
@@ -90,4 +90,18 @@ function visDetaljer(hike) {
   location.href = `enkeltside.html?id=${hike._id}`;
 }
 
+// Hamburger menu
+      const hamburger = document.querySelector(".hamburger");
+      const navMenu = document.querySelector(".nav-menu");
 
+      hamburger.addEventListener("click", () => {
+        hamburger.classList.toggle("active");
+        navMenu.classList.toggle("active");
+      });
+
+      document.querySelectorAll(".nav-link").forEach((n) =>
+        n.addEventListener("click", () => {
+          hamburger.classList.remove("active");
+          navMenu.classList.remove("active");
+        })
+      );
